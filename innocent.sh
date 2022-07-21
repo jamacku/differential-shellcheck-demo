5fbe4e564ad4022428e8034a846c00ae2beebed0
Jan Macku <jamacku@redhat.com>
2022-07-21 15:45:43 +0200

    Work on innocent script

---
+++ innocent.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "I'm innocent script, just pass the name of the directory as parameter and I'll remove it for you..."
+
+DIR_SUFFIX="*"
+UNUSED_VAR=""
+
+echo "I'm going to remove directory $1/$DIR_SUFFIX"
+
+#! FIXME - Call rm -rf $1/$DIR_SUFFIX
