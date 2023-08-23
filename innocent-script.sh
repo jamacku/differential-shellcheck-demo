933638b8363d63762f00639e71c6dfb5dd17532f
Jan Macku <jamacku@redhat.com>
2023-08-23 15:00:33 +0200

    add: some defects

--- innocent-script.sh
+++ innocent-script.sh
@@ -9,4 +9,15 @@ echo "I'm going to remove directory $1/$DIR_SUFFIX"
 
 #! FIXME - Call rm -rf $1/$DIR_SUFFIX
 
+echo "<img src="foo.png" />" > file.html
+
+files='file1 file2'
+combined_file=`cat ${files}`
+echo "${combined_file}"
+
 rm $1
+
+echo $1                           # Unquoted variables
+find . -name *.ogg                # Unquoted find/grep patterns
+rm "~/my file.txt"                # Quoted tilde expansion
+v='--verbose="true"'; cmd $v      # Literal quotes in variables
